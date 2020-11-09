e2bd8826e92700135fb0ba55a411602c6e02db3e
DaikiTanak <daiki.yosky@gmail.com>
2020-11-09 11:07:34 +0900

    add new file

--- setup_contest.sh
+++ setup_contest.sh
@@ -4,23 +4,33 @@ touch "$1"/a.py
 touch "$1"/b.py
 touch "$1"/c.py
 touch "$1"/d.py
+touch "$1"/e.py
+touch "$1"/f.py
 
 echo "# N = int(input())" >> "$1"/a.py
 echo "# N = int(input())" >> "$1"/b.py
 echo "# N = int(input())" >> "$1"/c.py
 echo "# N = int(input())" >> "$1"/d.py
+echo "# N = int(input())" >> "$1"/e.py
+echo "# N = int(input())" >> "$1"/f.py
 
 echo "# S = input()" >> "$1"/a.py
 echo "# S = input()" >> "$1"/b.py
 echo "# S = input()" >> "$1"/c.py
 echo "# S = input()" >> "$1"/d.py
+echo "# S = input()" >> "$1"/e.py
+echo "# S = input()" >> "$1"/f.py
 
 echo "# a_li = list(map(int, input().split()))" >> "$1"/a.py
 echo "# a_li = list(map(int, input().split()))" >> "$1"/b.py
 echo "# a_li = list(map(int, input().split()))" >> "$1"/c.py
 echo "# a_li = list(map(int, input().split()))" >> "$1"/d.py
+echo "# a_li = list(map(int, input().split()))" >> "$1"/e.py
+echo "# a_li = list(map(int, input().split()))" >> "$1"/f.py
 
 echo "# N, M = map(int, input().split())" >> "$1"/a.py
 echo "# N, M = map(int, input().split())" >> "$1"/b.py
 echo "# N, M = map(int, input().split())" >> "$1"/c.py
 echo "# N, M = map(int, input().split())" >> "$1"/d.py
+echo "# N, M = map(int, input().split())" >> "$1"/e.py
+echo "# N, M = map(int, input().split())" >> "$1"/f.py
